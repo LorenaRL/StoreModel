b96c148ac1a141af0da757fe676615423ae9887c
Lima <Lima@192.168.1.14>
2019-06-02 13:03:45 -0300

    refs #20190602 -  add arquivo php para deletar dados da tabela
    Merge origin/master

---
+++ PHP/deletaDadosTabela.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+    include('./conexaoCbancoCriado.php');
+    // apagar registro
+$sql = "DELETE FROM usuario WHERE id_login =2019009";
+if ($conecta->query($sql) === TRUE) {
+echo "Registro apagado com sucesso<br>";
+} else {
+echo "Erro ao apagar o registro: " . $conecta->error."<br>";
+}
+$conecta->close();
+?>
